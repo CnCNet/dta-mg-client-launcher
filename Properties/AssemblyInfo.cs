097d0299753b9d2f30b84971eb35f8d0bf8c75fb
CC-RaVaGe <54605063+CC-RaVaGe@users.noreply.github.com>
2022-04-22 00:47:56 +0100

    Updated Assembly Info.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DTA Launcher")]
-[assembly: AssemblyDescription("Launcher for Dawn of the Tiberium Age CnCNet Client")]
+[assembly: AssemblyTitle("Yuri's Revenge Redux")]
+[assembly: AssemblyDescription("Launcher for Yuri's Revenge Redux Client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dawn of the Tiberium Age")]
-[assembly: AssemblyCopyright("Copyright © Rampastring 2019")]
+[assembly: AssemblyCompany("Team Redux")]
+[assembly: AssemblyProduct("Yuri's Revenge Redux")]
+[assembly: AssemblyCopyright("Copyright © Rampastring 2019, Team Redux 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
