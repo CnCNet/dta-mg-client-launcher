092ccd778dea0cbf4cbc0673107d4c16d200ca5c
Sad Pencil <sadpencil@outlook.com>
2023-02-18 02:25:50 +0800

    Make NotifyPropertyChangedBase abstract

--- NotifyPropertyChangedBase.cs
+++ NotifyPropertyChangedBase.cs
@@ -3,7 +3,7 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-public class NotifyPropertyChangedBase : INotifyPropertyChanged
+public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler? PropertyChanged;
 
