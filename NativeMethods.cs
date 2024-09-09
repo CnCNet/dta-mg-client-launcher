25926e5c4cdcc8541d82d4bb1ef65293fb0e167c
SadPencil <sadpencil@outlook.com>
2024-09-09 23:02:15 +0800

    Update condition of DllImportSearchPath

--- NativeMethods.cs
+++ NativeMethods.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 internal static class NativeMethods
 {
     [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-#if NET45_OR_GREATER
+#if NET45_OR_GREATER || NETSTANDARD || NET
     [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
 #endif
     [return: MarshalAs(UnmanagedType.Bool)]
